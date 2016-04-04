d0f7b98db88731f1f4cb44b21534b67653b91586
Guin <guinifer.k@gmail.com>
2016-04-04 07:02:49 -0400

    input parsing finished

--- salestax.rb
+++ salestax.rb
@@ -1,24 +1,20 @@
 
-
-
 class Item
 
-  attr_accessor :catergory, :name, :imported
+  attr_accessor :catergory, :name, :imported, :tax, :excemptions
 
-  attr_reader :tax, :excemptions
+  attr_reader
 
+  #for this project I am going to set up the excemptions manually
+  #I hope that the add and end excemptions method show how easy it is to keep a list of
+  #tax excempt items, in a larger scale version this list would be stored somewhere and mainted
   @@excemptions = ['books', 'food', 'medical products']
 
-  def initialize(catergory, description,imported)
-    @catergory = catergory
+  def initialize(description, imported)
+    @catergory = set_catergory(description)
     @imported = imported
-    @tax = Object.const_get(set_tax_type).new(false)
-    #for this project I am going to set up the excemptions manually
-    #I hope that the add and end excemptions method show how easy it is to keep a list of
-    #tax excempt items
-
+    @tax = Object.const_get(set_tax_type).new(is_excempt?)
   end
-
 
   def self.add_excemption(excemption)
     @@excemptions << excemption
@@ -28,16 +24,24 @@ class Item
     @@excemptions.delete excemption
   end
 
+  def self.return_excemptions
+    @@excemptions
+  end
+
   private
+  def set_catergory(item)
+    #in a better designed system this would be captured on data entry
+    print "Please enter the category that #{item} belongs to : "
+    gets.chomp
+  end
 
   def set_tax_type
     return 'Import_Tax' if imported
     'Tax'
   end
 
-
   def is_excempt?
-    @@excemptions.find catergory
+    @@excemptions.find {|i| i == catergory}
   end
 
 end
@@ -61,5 +65,48 @@ class Import_Tax < Tax
     super(excempt)
     @tax_rate = @tax_rate + 0.05
   end
+end
+
+class Shopping_cart
+  attr_accessor :cart_contents
+
+  def initialize
+    @cart_contents = []
+  end
+
+  def add_to_cart(item)
+    cart_contents << item
+  end
+
 
 end
+
+
+#for testing purposes
+
+#the following code is for testing only
+input1 = "1 book at 12.49 \n1 music CD at 14.99 \n1 chocolate bar at 0.85"
+input2 = "1 imported box of chocolates at 10.00 \n1 imported bottle of perfume at 47.50"
+input3 = "1 imported bottle of perfume at 27.99 \n1 bottle of perfume at 18.99 \n1 packet of headache pills at 9.75 \n1 box of imported chocolates at 11.25"
+
+def split_input_string(input_string)
+  input_string.split
+end
+
+def fill_cart(input_string)
+  #takes one string and adds it to the cart
+  imported = true if input_string.include? "imported"
+
+  input_split = input_string.split
+
+  number_of_items = input_split.shift
+
+  price = input_split.pop
+  input_split.pop
+  price = price.to_f
+
+  description = input_split.join " "
+
+end
+
+puts fill_cart('1 imported bottle of perfume at 27.99 ')
--- test/test_salestax.rb
+++ test/test_salestax.rb
@@ -5,7 +5,10 @@ require './salestax'
 class TestSalestax < MiniTest::Test
 
   def setup
-    @item = Item.new('medicine','pills',false)
+    @item_imported_excempt = Item.new('pills',true)
+    @item_excempt = Item.new('cow parts', false)
+    @item_imported = Item.new('cow parts', true)
+    @item = Item.new('muffler', false)
     @taxable = Tax.new(false)
     @excempt = Tax.new(true)
     @import = Import_Tax.new(false)
@@ -14,14 +17,25 @@ class TestSalestax < MiniTest::Test
 
   def test_add_excemption
     Item.add_excemption 'medicine'
-    assert_equal ['books', 'food', 'medical products', 'medicine'], Item.excemptions
+    assert_equal ['books', 'food', 'medical products', 'medicine'], Item.return_excemptions
   end
 
-def test_end_excemption
-  Item.end_excemption 'medicine'
-  assert_equal ['books', 'food', 'medical products'], Item.excemptions
-end
-
+  def test_end_excemption
+    Item.end_excemption 'medicine'
+    assert_equal ['books', 'food', 'medical products'], Item.return_excemptions
+  end
+  def test_imported_tax_rate
+    assert_equal 0.15, @item_imported.tax.tax_rate.round(2)
+  end
+  def test_excempt_imported_tax_rate
+    assert_equal 0.05, @item_imported_excempt.tax.tax_rate.round(2)
+  end
+  def test_excempt_tax_rate
+    assert_equal 0, @item_excempt.tax.tax_rate.round(2)
+  end
+  def test_tax_rate
+    assert_equal 0.1, @item.tax.tax_rate.round(2)
+  end
 
 
   def test_tax_one_dollar
